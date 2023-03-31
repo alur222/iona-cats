c2fb29efcebb7a803e848f8a8d70fbe5c369b19d
Raul Torrefiel JR <aluroticooo@gmail.com>
2023-04-01 04:21:22 +0800

    Modify App to use context providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import BreedsProvider from './context/Breeds';
 import Home from './screens/Home';
 import NotFound from './screens/404';
 import Cat from './screens/Cat';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/cat/:id" element={<Cat />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <BreedsProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cat/:id" element={<Cat />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BreedsProvider>
   );
 }
 
